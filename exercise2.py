d3077452fd7eb83b59cf398977cffd98dd969c79
claravalls <claravll17@gmail.com>
2022-04-30 17:38:47 +0200

    Create unifed script for exercise 2 and modify policy cclass

---
+++ exercise2.py
@@ -0,0 +1,48 @@
+# Code that checks the number of passwords in a file that comply the specified policy
+# Each line has the following structure:
+#          <min_times>-<max_times> <letter>: <password>
+
+import sys
+import policy
+
+
+def main(args):
+    """
+    Main function
+
+    Arguments:
+    :param args: 
+        1 - path of the input file
+        2 - type of password check
+    """
+    correct_counter = 0
+
+    if len(args) < 2:
+        print("Missing input file argument")
+        return 1
+
+    input_file = args[0]
+    policy_type = int(args[1])
+
+    # read the input file line by line
+    with open(input_file, "r") as file:
+        for line in file:
+            # remove \n character
+            line = line.strip()
+
+            [max_min, target, password] = line.split(" ")
+            [min, max] = max_min.split("-")
+
+            # create a policy with the line info
+            rules = policy.Policy(int(min), int(max), target[0], password)
+
+            # check if the password complies with the policy
+
+            if rules.check_password(policy_type):
+                correct_counter += 1
+
+    print("{} correct passwords.".format(correct_counter))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- policy.py
+++ policy.py
@@ -1,5 +1,5 @@
 class Policy:
-    def __init__(self, min_times, max_times, target, password):
+    def __init__(self, first_num, second_num, target, password):
         """
         Constructor class
 
@@ -8,20 +8,28 @@ class Policy:
         :param target: letter to check in the password
         :param password: password to check
         """
-        self.min_times = min_times
-        self.max_times = max_times
+        self.first_num = first_num
+        self.second_num = second_num
         self.target = target
         self.password = password
 
-    def check_password_1(self):
+    def check_password(self, policy):
         """
         Check if the password follows the specified policy
+
+        Aruments
+        :param policy: policy type (1 or 2) to apply
         """
         counter = 0
-        for letter in self.password:
-            if letter == self.target:
-                counter += 1
 
-        # check if the target letter appears the specified number of times
-        times = range(self.min_times, self.max_times + 1)
-        return counter in times
+        if policy == 1:
+            for letter in self.password:
+                if letter == self.target:
+                    counter += 1
+
+            # check if the target letter appears the specified number of times
+            times = range(self.first_num, self.second_num + 1)
+            return counter in times
+
+        else:
+            return (self.password[self.first_num - 1] == self.target) != (self.password[self.second_num - 1] == self.target)
