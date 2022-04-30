68b1a49125c5236251fef6fe92a56a479d591da1
claravalls <claravll17@gmail.com>
2022-04-30 14:08:42 +0200

    Exercise 2: Create Policy class with constructor and a function to check the password

--- exercise2.1.py
+++ exercise2.1.py
@@ -2,6 +2,21 @@
 import sys
 
 
+class Policy:
+    def __init__(self, max_times, min_times, letter, password):
+        self.times = [max_times, min_times]
+        self.target = letter
+        self.password = password
+
+    def check_password(self):
+        counter = 0
+        for letter in self.password:
+            if letter == self.target:
+                counter += 1
+
+        return self.times[0] >= counter and counter <= self.times[1]
+
+
 def main(args):
     print(args)
 
