4befa8dc67e46eda8dbf8d57d45524ff5b5144da
claravalls <claravll17@gmail.com>
2022-04-30 13:56:37 +0200

    Exercise 1 commented

--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,5 @@
-#
+# Code that prints a list of numbers from 1 to 100 changing multiples of three
+# for "cyber", multiples of 5 for "click" and multiples of both for "cyberclick"
 
 def main():
     for i in range(1, 101):
@@ -8,6 +9,7 @@ def main():
         if i % 5 == 0:
             text += "click"
 
+        # if none of the conditions are met, print the number
         msg = i if text == "" else text
         print(msg)
 
