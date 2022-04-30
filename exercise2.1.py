362f0f44b023a8eb889774692e5d5b0f698da1bb
claravalls <claravll17@gmail.com>
2022-04-30 16:49:09 +0200

    Adding function comments

--- exercise2.1.py
+++ exercise2.1.py
@@ -1,38 +1,64 @@
-#
+# Code that checks the number of passwords in a file that comply the specified policy
+# Each line has the following structure:
+#          <min_times>-<max_times> <letter>: <password>
+
 import sys
 
 
 class Policy:
-    def __init__(self, max_times, min_times, letter, password):
-        self.times = [max_times, min_times]
-        self.target = letter
+    def __init__(self, min_times, max_times, target, password):
+        """
+        Constructor class
+
+        :param min_times: minimum number of times the letter has to appear
+        :param max_times: maximum numner of times the letter has to appear
+        :param target: letter to check in the password
+        :param password: password to check
+        """
+        # add 1 value to max_times to include it in range
+        self.times = range(min_times, max_times + 1)
+        self.target = target
         self.password = password
 
     def check_password(self):
+        """
+        Check if the password follows the specified policy
+        """
         counter = 0
         for letter in self.password:
             if letter == self.target:
                 counter += 1
 
-        return self.times[0] >= counter and counter <= self.times[1]
+        # check if the target letter appears the specified number of times
+        return counter in self.times
 
 
 def main(args):
+    """
+    Main function
+
+    Arguments:
+    :param args: path of the input file
+    """
     correct_counter = 0
 
     if len(args) < 1:
         print("Missing input file argument")
         return 1
 
+    # read the input file line by line
     with open(args[0], "r") as file:
         for line in file:
             # remove \n character
             line = line.strip()
 
             [max_min, target, password] = line.split(" ")
-            [max, min] = max_min.split("-")
+            [min, max] = max_min.split("-")
 
-            policy = Policy(int(max), int(min), target[0], password)
+            # create a policy with the line info
+            policy = Policy(int(min), int(max), target[0], password)
+
+            # check if the password complies with the policy
             if policy.check_password():
                 correct_counter += 1
 
